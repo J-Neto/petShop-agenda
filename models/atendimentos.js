124463685994ab021629292826b38a38e5cf170a
José <ribeirojoseph44@gmail.com>
2022-03-07 11:31:07 -0300

    Listar atendimentos e atendimento específico

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -42,6 +42,30 @@ class Atendimento{
 
 
     }
+
+    lista(res){
+        const sql = 'SELECT * FROM Atendimentos';
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else{
+                res.status(200).json(resultados[0]);
+            }
+        })
+    }
 }
 
 module.exports = new Atendimento;
